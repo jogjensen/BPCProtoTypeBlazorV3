58668f7d1f7ebd7cbce3cfb966dc264059e84a17
Philip Lind Jensen <jogjensen@hotmail.com>
2020-11-18 01:24:27 +0100

    Test2

--- BPCProtoTypeBlazorV3/Server/Controllers/WeatherForecastController.cs
+++ BPCProtoTypeBlazorV3/Server/Controllers/WeatherForecastController.cs
@@ -16,6 +16,7 @@ namespace BPCProtoTypeBlazorV3.Server.Controllers
     {
 
         //This is a github test
+        //This is a github test 2
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
